ab1e997625d57cd3799105e0fd3ed3b5913e9d94
Jeroen Keppens <jeroen@smartwebproducts.com>
2019-02-23 14:03:25 +0100

    php version

--- src/Exception/MakeExtractable.php
+++ src/Exception/MakeExtractable.php
@@ -10,12 +10,18 @@ trait MakeExtractable
      */
     public function getArrayCopy(array $options = []): array
     {
+        $trace = str_replace("\r", "\n", $this->getTraceAsString());
+        if ($this->getPrevious()) {
+            $trace .= PHP_EOL . str_repeat('-', 20) . PHP_EOL;
+            $trace .= str_replace("\r", "\n", $this->getPrevious()->getTraceAsString());
+        }
+
         return [
             'message' => $this->getMessage(),
             'code'    => $this->getCode(),
             'file'    => $this->getFile(),
             'line'    => $this->getLine(),
-            'trace'   => $this->getTrace(),
+            'trace'   => $trace,
         ];
     }
 }
